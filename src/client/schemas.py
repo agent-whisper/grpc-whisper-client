9c41e1bfe1afbd10ae7efd4f8dde568fc01150ad
Fariz Tumbuan <fariz.tumbuan@gmail.com>
2023-05-04 11:14:14 +0900

    Added output formatting

--- src/client/schemas.py
+++ src/client/schemas.py
@@ -1,6 +1,8 @@
+import json
+
 from typing import List
 from pydantic import BaseModel
-
+from datetime import timedelta
 from src.generated.result_pb2 import Segment, TranscriptionResult
 
 
@@ -36,6 +38,26 @@ class Segment(BaseModel):
             no_speech_prob=obj.no_speech_prob,
         )
 
+    def output_srt(self) -> str:
+        output = [
+            f"{self.id + 1}",
+            f"{self._format_timing(self.start)} --> {self._format_timing(self.end)}",
+            self.text,
+        ]
+        return "\n".join(output)
+
+    def _format_timing(self, total_seconds: float):
+        td = timedelta(seconds=total_seconds)
+        if td.microseconds > 0:
+            microseconds = str(td.microseconds)[:3]
+            timing = f"{str(timedelta(seconds=td.seconds))},{microseconds}"
+        else:
+            timing = f"{str(td)},000"
+        if total_seconds < 36_000:
+            return f"0{timing}"
+        else:
+            return timing
+
 
 class Transcription(BaseModel):
     text: str
@@ -49,3 +71,27 @@ class Transcription(BaseModel):
             language=obj.language,
             segments=[Segment.from_proto(segment) for segment in obj.segments],
         )
+
+    def output_json(
+        self,
+        *,
+        output_dir: str = None,
+        ensure_ascii: bool = False,
+        encoding: str = "utf-8",
+    ):
+        if output_dir:
+            with open(output_dir, "w", encoding=encoding) as f:
+                json.dump(self.dict(), f, ensure_ascii=ensure_ascii, indent=4)
+        else:
+            return json.dumps(self.dict(), ensure_ascii=ensure_ascii, indent=4)
+
+    def output_text(self, *, delimiter=" "):
+        return delimiter.join(segment.text for segment in self.segments)
+
+    def output_srt(self, *, output_dir: str = None, encoding: str = "utf-8"):
+        output = "\n\n".join([segment.output_srt() for segment in self.segments])
+        if output_dir:
+            with open(output_dir, "w", encoding=encoding) as f:
+                f.write(output)
+        else:
+            return output
